41a19d2fd39b02392bb1ce2ab6391c741aabd5d4
redarmr <david.ren@nyu.edu>
2026-05-22 13:09:54 -0400

    Add generated app file: src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Ocyber: Threat Intelligence Dashboard</h1>
+      <p>Fetch and visualize cyber threat data.</p>
+    </div>
+  );
+};
+
+export default App;
